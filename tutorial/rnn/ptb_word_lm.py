84582c886bcb06ffccd37ebd29b30eb48129a6cc
leidelong <leidelong@meituan.com>
2017-01-05 16:47:24 +0800

    add desc

--- tutorial/rnn/ptb_word_lm.py
+++ tutorial/rnn/ptb_word_lm.py
@@ -2,21 +2,6 @@
 # encoding: utf-8
 
 
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
 """
 @version: ??
 @author: leidelong
@@ -57,6 +42,9 @@ $ wget http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
 $ tar xvf simple-examples.tgz
 To run:
 $ python ptb_word_lm.py --data_path=simple-examples/data/
+使用方法：
+$ python ptb_word_lm.py --data_path=/Users/leidelong/data/simple-examples/data/ --model small
+
 """
 from __future__ import absolute_import
 from __future__ import division
