69d3ecbfdf435779dd32eba0e78850eb7e1b6f78
leidelong <leidelong@meituan.com>
2017-02-08 18:12:20 +0800

    add model_contrast & neural_style

---
+++ train_model/neural_style/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: leidelong
+@license: Apache Licence 
+@contact: leidl8907@gmail.com
+@site: https://github.com/JoneNash
+@software: PyCharm Community Edition
+@time: 2017/2/5 下午8:54
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
